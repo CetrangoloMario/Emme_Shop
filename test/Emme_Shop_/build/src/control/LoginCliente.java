10bb5e8752f33deb12b06e856bba60cf9af5fb4b
vincenzomonte <55898264+vincenzomonte@users.noreply.github.com>
2020-01-07 22:46:29 +0100

    LoginCliente

---
+++ test/Emme_Shop_/build/src/control/LoginCliente.java
@@ -0,0 +1,38 @@
+package control;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+/**
+ * Servlet implementation class LoginCliente
+ */
+@WebServlet("/LoginCliente")
+public class LoginCliente extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginCliente() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//ricordarsi di caricare il carrello in sessione e modificare il logout per andare a vedere il carrello in db ....
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+}
